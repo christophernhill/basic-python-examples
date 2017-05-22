0bf134e77227df263d047628e8a2228a5a92abde
christophernhill <cnh@mit.edu>
2017-05-22 14:26:40 -0400

    Update gendata.py

--- verification/tutorial_barotropic_gyre/input/gendata.py
+++ verification/tutorial_barotropic_gyre/input/gendata.py
@@ -21,22 +21,22 @@ def gendata():
     ny = 60
 
     # Generate bathymetry with flat bottom at z=-Ho
-    h = -Ho*np.ones((nx, ny))
+    h = -Ho*np.ones((ny, nx))
     # Walls
     h[-1, :] = 0
     h[:, -1] = 0
-    with open('topog.box','wb') as fid:
-        h.astype(ofmt).tofile(fid,"")
+    with open('topog.box', 'wb') as fid:
+        h.astype(ofmt).tofile(fid, "")
 
     # Generate zonal wind-stress file that is sin with period of dmain north-south extent
     # and maximum of 0.1 Nm^{2}
     tauMax = 0.1
-    x = (np.arange(1,nx+1)-0.5) / (nx-1.) # nx-1 accounts for a solid wall
-    y = (np.arange(1,ny+1)-0.5) / (ny-1.) # ny-1 accounts for a solid wall
-    X, Y = np.meshgrid(x,y)
+    x = (np.arange(1, nx+1)-0.5) / (nx-1.) # nx-1 accounts for a solid wall
+    y = (np.arange(1, ny+1)-0.5) / (ny-1.) # ny-1 accounts for a solid wall
+    X, Y = np.meshgrid(x, y)
     tau = tauMax*np.sin(np.pi*Y)
-    with open('windx.sin_y','wb') as fid:
-        tau.astype(ofmt).tofile(fid,"")
+    with open('windx.sin_y', 'wb') as fid:
+        tau.astype(ofmt).tofile(fid, "")
 
 if __name__ == '__main__':
     gendata()
